5679ad2a260dc8aa1371d80c90d945310b111f40
ckma23 <curtiskma@gmail.com>
2018-01-01 12:18:44 -0800

    testing Yelp API on heroku

--- services/yelpservice.js
+++ services/yelpservice.js
@@ -2,13 +2,24 @@ var yelpApiSetup = require ('../config/yelpapi.json')
 
 'use strict';
 
+const yelp = require('yelp-fusion');
+
 var yelpApiParams = {
   apiKey: process.env.YELPAPIKEY || yelpApiSetup.apiKey,
 };
 
+const client = yelp.client(yelpApiParams.apiKey);
 
-function getYelphandler(){
-  console.log("Working")
+function getYelphandler(locationString,callback){
+  console.log(yelpApiSetup.apiKey)
+  const searchRequest = {location: locationString}
+  client.search(searchRequest).then(response =>{
+    const results = response.jsonBody.businesses;
+    console.log("Search Returned",results);
+    callback(results);
+  }).catch(error => {
+    console.log(error);
+  });
 }
 
 module.exports.getYelphandler = getYelphandler;

4ab5232ea0be36687fb8057bd89955a2e1a9ba3d
ckma23 <curtiskma@gmail.com>
2018-01-01 13:18:42 -0800

    Using dotenv to handle API key in dev environment and in production

--- services/yelpservice.js
+++ services/yelpservice.js
@@ -1,17 +1,21 @@
-var yelpApiSetup = require ('../config/yelpapi.json')
+// var yelpApiSetup = require ('../config/yelpapi.json')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+  console.log("The process environment is not in production")
+}
 
 'use strict';
 const yelp = require('yelp-fusion');
 
 var yelpApiParams = {
-  apiKey: process.env.YELPAPIKEY || yelpApiSetup.apiKey,
+  apiKey: process.env.YELPAPIKEY || process.env.apiKey,
 };
 
 
 const client = yelp.client(yelpApiParams.apiKey);
 
 function getYelphandler(locationString,callback){
-  console.log(yelpApiSetup.apiKey)
   const searchRequest = {location: locationString}
   client.search(searchRequest).then(response =>{
     const results = response.jsonBody.businesses;
